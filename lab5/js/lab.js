aa814d9e62cf9170480e431376f0cf26e4554559
Jorge0sorio <josorio3@ucsc.edu>
2022-04-19 16:46:53 -0700

    Lab 5 First Push

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ * Author:    Jorge
+ * Created:   4.19.2022
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ // define variables
+var make = "Toyota";
+var model = "Matrix";
+var year = 2011;
+var currentYear = 2022;
+var color = "Brown";
+var ownIt = false;
+
+ //calculate
+var age = currentYear - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Age of Car: " + age + "<br>");
